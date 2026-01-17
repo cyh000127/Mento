f6656b35d52f9c119a1cd421d25a8d93b83f1553
윤섭 <54024213+F-UMI@users.noreply.github.com>
2026-01-18 03:22:32 +0900

    🎉 init: Spring Boot 애플리케이션 초기 설정 추가
    Ref: #S14P11A704-29, #S14P11A704-30

---
+++ backend/src/main/java/com/mready/MreadyApplication.java
@@ -0,0 +1,13 @@
+package com.mready;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MreadyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MreadyApplication.class, args);
+	}
+
+}
