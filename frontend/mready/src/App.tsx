7532a44b101b2ef0179ed918c3bcffac236a848f
kjm <kgmline2@naver.com>
2026-01-22 16:59:29 +0900

    🎨 chore:코드 스타일 통일을 위한 ESLint, Prettier 설정
    - ESLint 설정
    - Prettier 설정
    
    Ref: #S14P11A704-117

--- frontend/mready/src/App.tsx
+++ frontend/mready/src/App.tsx
@@ -1,9 +1,11 @@
 function App() {
   return (
     <div className="min-h-screen bg-black flex items-center justify-center">
-      <div className="text-4xl font-extrabold text-red-500">Tailwind v4 Ready</div>
+      <div className="text-4xl font-extrabold text-red-500">
+        Tailwind v4 Ready
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
