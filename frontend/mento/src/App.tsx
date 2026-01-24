302e0e90918fa7d412027eab2b3e8c528fe478f4
kjm <kgmline2@naver.com>
2026-01-24 18:51:52 +0900

    ✨ feat: 라우터 설정
    - Data Router 방식으로 라우터 설정
    - 임시 Home, MyPage 생성
    -@ 알리아스 설정
    
    Ref. #S14P11A704-119

--- frontend/mento/src/App.tsx
+++ frontend/mento/src/App.tsx
@@ -1,11 +1,13 @@
-function App() {
+import { Outlet } from 'react-router-dom'
+
+export default function App() {
   return (
-    <div className="min-h-screen bg-black flex items-center justify-center">
-      <div className="text-4xl font-extrabold text-red-500">
-        Tailwind v4 Ready
-      </div>
+    <div>
+      <header>MENTO Header</header>
+
+      <main>
+        <Outlet />
+      </main>
     </div>
   )
 }
-
-export default App
