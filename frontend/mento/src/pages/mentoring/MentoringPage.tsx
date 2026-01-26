3e50fd8e2f532981a6d74a8e96125cc0a5119c26
kjm <kgmline2@naver.com>
2026-01-26 14:03:55 +0900

    ✨ feat: 멘토링 페이지에 상담 예약 페이지 연결
    - 멘토 생성 섹션 제거
    - 멘토링 하러 가기 버튼 추가 & 해당 버튼에 상담 예약 페이지로 링크
    
    Ref. #S14P11A704-159

--- frontend/mento/src/pages/mentoring/MentoringPage.tsx
+++ frontend/mento/src/pages/mentoring/MentoringPage.tsx
@@ -1,13 +1,11 @@
 import { MentoringHero } from "@/components/mentoring/mentoring-hero"
 import { HowMentoringWorks } from "@/components/mentoring/how-mentoring-works"
-import { MentorGrid } from "@/components/mentoring/mentor-grid"
 import { MentoringFaq } from "@/components/mentoring/mentoring-faq"
 
 export default function MentoringPage() {
   return (
     <>
       <MentoringHero />
-      <MentorGrid />
       <HowMentoringWorks />
       <MentoringFaq />
     </>
