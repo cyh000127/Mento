cbb6737cf8443406a1cef6fb07b5b2d5b54527d5
신윤섭 <zksdbstjq@gmail.com>
2026-01-31 18:44:24 +0900

    ✨ feat: 아이템 생성 API 엔드포인트 추가
    - POST /api/v1/items 엔드포인트 구현
    - 인증 유저 기반 아이템 생성 서비스 연동
    
    Ref: #S14P11A704-213, #S14P11A704-202

---
+++ backend/src/main/java/com/mento/domain/item/controller/command/ItemCommandController.java
@@ -0,0 +1,38 @@
+package com.mento.domain.item.controller.command;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mento.common.auth.principal.AuthenticatedUser;
+import com.mento.common.response.BaseResponse;
+import com.mento.common.util.ResponseUtils;
+import com.mento.domain.item.dto.common.ItemInfoResDto;
+import com.mento.domain.item.dto.request.UserItemAddReqDto;
+import com.mento.domain.item.service.facade.ItemFacadeService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+@Tag(name = "User Item", description = "유저 아이템 관리 API")
+@RestController
+@RequestMapping("/api/v1/items")
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class ItemCommandController {
+
+	private final ItemFacadeService itemFacadeService;
+
+	@PostMapping
+	public ResponseEntity<BaseResponse<ItemInfoResDto>> addItem(
+		@AuthenticationPrincipal AuthenticatedUser authUser,
+		@RequestBody UserItemAddReqDto reqDto
+	) {
+		ItemInfoResDto response = itemFacadeService.addUserItem(authUser.getId(), reqDto);
+		return ResponseUtils.ok(response);
+	}
+
+}
