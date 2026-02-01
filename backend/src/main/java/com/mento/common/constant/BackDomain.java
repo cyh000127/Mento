ae6f1ed10e9607342f3d21d67e7a8ed78dc4f3b2
okccc5 <okccc5@naver.com>
2026-02-01 16:56:34 +0900

    🚑️ hotfix: Backend 도메인 주소 긴급 변경

--- backend/src/main/java/com/mento/common/constant/BackDomain.java
+++ backend/src/main/java/com/mento/common/constant/BackDomain.java
@@ -9,7 +9,7 @@ import lombok.RequiredArgsConstructor;
 public enum BackDomain {
 
 	LOCAL("http://localhost:8080", "백엔드 로컬 도메인"),
-	PROD("https://i14a704.p.io", "백엔드 배포 도메인");
+	PROD("https://i14a704.p.ssafy.io", "백엔드 배포 도메인");
 
 	private final String url;
 	private final String description;
