ff5edf0eb21e055908862e4e63519f360c514346
okccc5 <okccc5@naver.com>
2026-02-03 17:27:19 +0900

    ♻️ refactor: Mentor 로그인 코드 수정
    - 코드 스타일에 맞게 코드 수정
    
    Ref. S14P11A704-340

--- backend/src/main/java/com/mento/domain/auth/service/MentorAuthFacadeService.java
+++ backend/src/main/java/com/mento/domain/auth/service/MentorAuthFacadeService.java
@@ -5,10 +5,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.mento.common.auth.dto.Token;
 import com.mento.common.auth.jwt.JwtTokenProvider;
+import com.mento.domain.auth.converter.AuthConverter;
 import com.mento.domain.auth.dto.request.MentorLoginReqDto;
 import com.mento.domain.auth.service.command.MentorAuthCommandService;
 import com.mento.domain.mentor.entity.Mentor;
-import com.mento.domain.user.entity.Role;
 import com.mento.domain.user.entity.User;
 
 import lombok.RequiredArgsConstructor;
@@ -24,14 +24,7 @@ public class MentorAuthFacadeService {
 	public Token login(final MentorLoginReqDto reqDto) {
 		Mentor mentor = mentorAuthCommandService.login(reqDto);
 
-		User dummyUser = User.builder()
-			.id(mentor.getId())
-			.name(mentor.getName())
-			.email(mentor.getLoginId() + "@mentor.com")
-			.role(Role.MENTOR)
-			.password(mentor.getPassword()) 
-			.kakaoId("mentor_" + mentor.getId())
-			.build();
+		User dummyUser = AuthConverter.toUser(mentor);
 
 		return jwtTokenProvider.createToken(dummyUser);
 	}
