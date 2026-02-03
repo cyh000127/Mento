36083f57ca775ffd9a2487f3aebfbdc24d542299
okccc5 <okccc5@naver.com>
2026-02-03 13:45:56 +0900

    ♻️ refactor: 코드 지침에 맞게 코드 수정
    - CQRS, Facade 규칙에 맞게 코드 수정
    
    Ref. S14P11A704-340

---
+++ backend/src/main/java/com/mento/domain/auth/service/MentorAuthFacadeService.java
@@ -0,0 +1,38 @@
+package com.mento.domain.auth.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mento.common.auth.dto.Token;
+import com.mento.common.auth.jwt.JwtTokenProvider;
+import com.mento.domain.auth.dto.request.MentorLoginReqDto;
+import com.mento.domain.auth.service.command.MentorAuthCommandService;
+import com.mento.domain.mentor.entity.Mentor;
+import com.mento.domain.user.entity.Role;
+import com.mento.domain.user.entity.User;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MentorAuthFacadeService {
+
+	private final MentorAuthCommandService mentorAuthCommandService;
+	private final JwtTokenProvider jwtTokenProvider;
+
+	@Transactional
+	public Token login(final MentorLoginReqDto reqDto) {
+		Mentor mentor = mentorAuthCommandService.login(reqDto);
+
+		User dummyUser = User.builder()
+			.id(mentor.getId())
+			.name(mentor.getName())
+			.email(mentor.getLoginId() + "@mentor.com") // Dummy email for token
+			.role(Role.MENTOR)
+			.password(mentor.getPassword()) // Not really needed for token but consistent with previous code
+			.kakaoId("mentor_" + mentor.getId())
+			.build();
+
+		return jwtTokenProvider.createToken(dummyUser);
+	}
+}
