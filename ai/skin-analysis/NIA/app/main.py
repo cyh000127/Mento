b50591b58f13023bb5725fd91edaf2db6724ea69
SeunghunKang <kangseunghun9927@gmail.com>
2026-02-06 13:55:23 +0900

    ✨feat: 배포 환경 설정, Docker 파일 설정
    - 속도를 위한 배포환경 파이썬 버전 업그레이드(3.12)

--- ai/skin-analysis/NIA/app/main.py
+++ ai/skin-analysis/NIA/app/main.py
@@ -108,4 +108,4 @@ def health_check():
 if __name__ == "__main__":
     import uvicorn
     # Run slightly differently for testing
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=4000, reload=True)
